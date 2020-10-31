6d4b52a1976f975a4e6ce2e4265fcc91ab78b496
Tiffany Bennett <tiffnixen@gmail.com>
2020-10-30 18:55:33 -0700

    Initial commit

---
+++ src/main/java/com/tiffnix/invisibleitemframes/InvisibleItemFrames.java
@@ -0,0 +1,17 @@
+package com.tiffnix.invisibleitemframes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InvisibleItemFrames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
